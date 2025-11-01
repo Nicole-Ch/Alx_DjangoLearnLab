be910b083ee920d824733c0a6886f920636533c6
Nicole Nereah <nicole@learning.git>
2025-11-01 19:40:44 +0300

    Resolvd

--- Introduction_to_Django/LibraryProject/bookshelf/admin.py
+++ Introduction_to_Django/LibraryProject/bookshelf/admin.py
@@ -1,3 +1,24 @@
 from django.contrib import admin
+from .models import Book
 
-# Register your models here.
+@admin.register(Book)
+class BookAdmin(admin.ModelAdmin):
+    # Display these fields in the list view
+    list_display = ('title', 'author', 'publication_year')
+    
+    # Add filters for these fields in the right sidebar
+    list_filter = ('publication_year', 'author')
+    
+    # Enable search for these fields
+    search_fields = ('title', 'author')
+    
+    # Optional: Group fields in the detail view
+    fieldsets = [
+        ('Basic Information', {
+            'fields': ['title', 'author']
+        }),
+        ('Publication Details', {
+            'fields': ['publication_year'],
+            'classes': ['collapse']  # Makes this section collapsible
+        }),
+    ]
