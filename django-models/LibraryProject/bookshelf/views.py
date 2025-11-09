a894a33b70b5a5519a58f092ff38d69c7c0d801f
Nicole Nereah <nicole@learning.git>
2025-11-09 14:53:27 +0300

    Add permission_required import to bookshelf views

--- django-models/LibraryProject/bookshelf/views.py
+++ django-models/LibraryProject/bookshelf/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+﻿from django.shortcuts import render
+from django.contrib.auth.decorators import permission_required
 
-# Create your views here.
+# Book management views with permission_required decorator
+@permission_required('relationship_app.can_add_book')
+def add_book(request):
+    return render(request, 'add_book.html')
+
+@permission_required('relationship_app.can_change_book')
+def edit_book(request, book_id):
+    return render(request, 'edit_book.html')
+
+@permission_required('relationship_app.can_delete_book')
+def delete_book(request, book_id):
+    return render(request, 'delete_book.html')
