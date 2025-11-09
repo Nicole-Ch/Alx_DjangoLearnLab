e107af01df43c02ca3533d5d86919691d8b8cda0
Nicole Nereah <nicole@learning.git>
2025-11-09 10:01:27 +0300

    Added Authentication

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path
-from .views import list_books, LibraryDetailView
+from .views import SignUpView, list_books, LibraryDetailView
+from django.contrib.auth.views import LoginView, LogoutView
+from django.urls import path
 
 urlpatterns = [
     # function-based view
@@ -7,4 +9,8 @@ urlpatterns = [
 
     # class-based view (use .as_view())
     path('library/<int:pk>/', LibraryDetailView.as_view(), name='library_detail'),
-]
+    path('login/', LoginView.as_view(template_name='registration/login.html'), name='login'),
+    path('signup/', SignUpView.as_view(), name='signup'),
+    path('logout/', LogoutView.as_view(), name='logout'),
+
+]     
