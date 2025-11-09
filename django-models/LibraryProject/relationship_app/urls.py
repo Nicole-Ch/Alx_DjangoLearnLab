da14bdf269d6044f5874bb5a3bb4e63f9c4c43c3
Nicole Nereah <nicole@learning.git>
2025-11-09 08:52:01 +0300

    Corrected views

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -1,10 +1,10 @@
-from django.contrib import admin
-from django.urls import path #Brings in the path function, which you use to map URL patterns to view functions.
-from . import views
+from django.urls import path
+from .views import list_books, LibraryDetailView
 
-urlpatterns =[
-   
-    path('books/', views.list_books, name='list_books'),  # function-based view
-    path('library/<int:pk>/', views.LibraryDetailView.as_view(), name='library_detail'),  # class-based view
+urlpatterns = [
+    # function-based view
+    path('books/', list_books, name='list_books'),
 
+    # class-based view (use .as_view())
+    path('library/<int:pk>/', LibraryDetailView.as_view(), name='library_detail'),
 ]
