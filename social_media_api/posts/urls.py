35975b93a221e4893aae7fca29532e72dc141330
Nicole Nereah <nicole@learning.git>
2025-12-12 11:52:36 +0300

    Created an accounts Authentication

--- social_media_api/posts/urls.py
+++ social_media_api/posts/urls.py
@@ -1,11 +1,13 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from .views import PostViewSets, CommentViewSets
+from . import views
 
 router = DefaultRouter()
 router.register(r'Posts', PostViewSets)
 router.register(r'comments', CommentViewSets)
 
-urlpattterns = [
-    path('', include(router.urls))
+urlpatterns = [
+    path('', include(router.urls)),
+    path('feed/', views.UserFeedView.as_view(), name='user-feed'),
 ]
--- social_media_api/social_media_api/urls.py
+++ social_media_api/social_media_api/urls.py
@@ -7,7 +7,7 @@ from accounts.views import FeedAPIView
 urlpatterns = [
         path('admin/', admin.site.urls),
         path('api/accounts/', include('accounts.urls')),
-          path('feed/', FeedAPIView.as_view(), name='feed'),
+          
         path('accounts/', include('accounts.urls')),
         path('api/', include('posts.urls')),
     ]
