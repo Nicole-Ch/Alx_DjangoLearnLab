424a5f6318dde1051be0db825d903fd813934257
Nicole Nereah <nicole@learning.git>
2025-12-12 09:41:50 +0300

    Created routers and urls

--- social_media_api/posts/views.py
+++ social_media_api/posts/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render
-from rest_framework import viewsets, permissions
+from rest_framework import viewsets, permissions , filters
 from rest_framework.permissions import IsAuthenticated
 from .permissions import IsOwnerOrReadOnly
+from .pagination import StandardResultsSetPagination
 from .models import Post, Comment
 from .serializers import PostSerializer, CommentSerializer
 
@@ -10,8 +11,23 @@ class PostViewSets(viewsets.ModelViewSet):
     queryset = Post.objects.all()
     serializer_class = PostSerializer
     permission_classes = [permissions.IsAuthenticated, IsOwnerOrReadOnly]
+    pagination_class = StandardResultsSetPagination
+    filter_backends = [filters.OrderingFilter, filters.SearchFilter]
+    search_fields = ['title' , 'content']
+    ordering_fields = ['created_at', 'updated-at']
+
+    def perform_create(self, serializer):
+        serializer.save(author=self.request.user)
 
 class CommentViewSets(viewsets.ModelViewSet):
     queryset = Comment.objects.all()
     serializer_class = CommentSerializer
     permission_classes = [permissions.IsAuthenticated, IsOwnerOrReadOnly]
+    pagination_class = StandardResultsSetPagination
+    filter_backends = [filters.OrderingFilter]
+   
+    ordering_fields = ['created_at']
+
+    def perform_create(self, serializer):
+        serializer.save(author=self.request.user)
+        #it’s a DRF hook that sets the post/comment author automatically to the currently authenticated user when the view creates a new object.
