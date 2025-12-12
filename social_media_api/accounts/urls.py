26cd2e8f8765cb13bdf8c1d2faae11c1b3ac06e6
Nicole Nereah <nicole@learning.git>
2025-12-12 11:42:08 +0300

    Created an accounts Authentication

--- social_media_api/accounts/urls.py
+++ social_media_api/accounts/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
 from .views import FollowUserAPIView, SignInView , RegisterView, ProfileAPIView, UnfollowUserAPIView
+from . import views
 
 
 urlpatterns = [
@@ -8,7 +9,11 @@ urlpatterns = [
     path('profile/', ProfileAPIView.as_view(), name='profile'),
 
 
-     path('follow/<int:user_id>/', FollowUserAPIView.as_view(), name='follow-user'),
-    path('unfollow/<int:user_id>/', UnfollowUserAPIView.as_view(), name='unfollow-user'),
+     path('follow/<int:user_id>/', views.FollowUserAPIView.as_view(), name='follow-user'),
+    path('unfollow/<int:user_id>/', views.UnfollowUserAPIView.as_view(), name='unfollow-user'),
+    path('follow-status/<int:user_id>/', views.UserFollowStatusAPIView.as_view(), name='follow-status'),
+    path('users/<int:user_id>/followers/', views.UserFollowersListAPIView.as_view(), name='user-followers'),
+    path('users/<int:user_id>/following/', views.UserFollowingListAPIView.as_view(), name='user-following'),
+    path('users/', views.UserListAPIView.as_view(), name='user-list'),
     
 ]
