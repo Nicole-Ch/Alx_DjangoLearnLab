692d9acd2571aec10fe1faa9d00760b3d8ad0b3d
Nicole Nereah <nicole@learning.git>
2025-12-12 09:02:01 +0300

    Created an models

---
+++ social_media_api/posts/models.py
@@ -0,0 +1,29 @@
+from django.db import models
+
+from django.conf import settings
+
+# Create your models here.
+class Post(models.Model):
+    author = models.ForeignKey(settings.AUTH_USER_MODEL, related_name="posts", on_delete=models.CASCADE)
+    title = models.CharField(max_length=100)
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        ordering = ["-created_at"]
+
+    def __str__(self):
+        f"{self.title} by {self.author}"    
+class Comment(models.Model):
+    post = models.ForeignKey(Post, related_name="comments",on_delete=models.CASCADE)
+    author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        ordering = ["-created_at"]
+
+    def __str__(self):
+        return f"Comment by {self.author} on post {self.post.id}"   
---
+++ social_media_api/posts/serializers.py
@@ -0,0 +1,19 @@
+from rest_framework import serializers
+from .models import Post, Comment
+
+
+class PostSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Post
+        fields = "__all__"
+
+    def validate_title(self,data):
+        if len(data) < 5:
+            raise serializers.ValidationError("Title should Have more characters")
+
+class CommentSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Comment
+        fields = "__all__"            
