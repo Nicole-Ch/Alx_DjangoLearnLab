fa9f4840af1bfe7834c54a218b1d43be56bc5ccc
Nicole Nereah <nicole@learning.git>
2025-12-13 11:13:41 +0300

    Added urls in posts for liking and unlikin

--- social_media_api/posts/urls.py
+++ social_media_api/posts/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import PostViewSets, CommentViewSets
+from .views import LikePostAPIView, PostViewSets, CommentViewSets, UnlikePostAPIView
 from . import views
 
 router = DefaultRouter()
@@ -10,4 +10,7 @@ router.register(r'comments', CommentViewSets)
 urlpatterns = [
     path('', include(router.urls)),
     path('feed/', views.UserFeedView.as_view(), name='user-feed'),
+
+    path('posts/<int:pk>/like/', LikePostAPIView.as_view(), name='like-post'),
+    path('posts/<int:pk>/unlike/', UnlikePostAPIView.as_view(), name='unlike-post'),
 ]
